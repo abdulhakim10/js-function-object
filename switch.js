9fdb52d57493c519478a6c2951b60b03059631c6
abdulhakim10 <s.abdulhakim10@gmail.com>
2022-08-05 16:47:11 +0600

    switch.js added

---
+++ switch.js
@@ -0,0 +1,30 @@
+const color = 'yellow';
+// if (color === 'green') {
+//     console.log('you are hulk frnd');
+// }
+// else if (color === 'red') {
+//     console.log('you are angry bird frnd');
+// }
+// else if (color === 'yellow') {
+//     console.lo
+//     g('you are himu frnd');
+// }
+// else {
+//     console.log('you are kala frnd');
+// }
+
+// switch
+switch (color) {
+    case 'green':
+        console.log('you are hulk');
+        break;
+    case 'red':
+        console.log('you are angry bird');
+        break;
+    case 'yellow':
+        console.log('you are himu');
+        break;
+    default:
+        console.log('you are kala patha');
+
+}
